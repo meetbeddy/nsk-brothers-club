25f69aa8b56f7c69655da7c4fe2445fc1b7db1ec
Obed Okpala <katlybedrick@example.com>
2024-03-25 15:50:42 +0100

    add emailjs functionality

--- src/landing/contact/Contact.tsx
+++ src/landing/contact/Contact.tsx
@@ -1,6 +1,45 @@
-
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
 
 function Contact() {
+    const form = useRef<HTMLFormElement>(null);
+
+    const SERVICE_ID: string = 'service_pw3rfyf';
+    const TEMPLATE_ID: string = "template_sax5gfv";
+    const PUBLIC_KEY: string = "byep6puCeHiqd5F3f";
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!form.current) return;
+
+
+        emailjs
+            .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
+                publicKey: PUBLIC_KEY,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+
+                    Swal.fire({
+                        icon: "success",
+                        title: "Successful",
+                        text: "message sent successfully!",
+                    });
+                },
+                (error) => {
+
+                    Swal.fire({
+                        icon: "error",
+                        title: "Message Not sent",
+                        text: error?.text
+                    });
+                },
+            );
+    };
+
     return (
         <section id="contact" className="contact">
             <div className="container" data-aos="fade-up">
@@ -8,7 +47,25 @@ function Contact() {
                     <h2>Contact</h2>
                     <p>Contact Us</p>
                 </div>
-                <div className="mapouter"><div className="gmap_canvas"><iframe className="gmap_iframe" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0} src="https://maps.google.com/maps?width=600&height=400&hl=en&q=No. 4 Canaan drive, Canaan Estate Life Camp Ext, Abuja&t=h&z=14&ie=UTF8&iwloc=B&output=embed" /><a href="https://strandsgame.net/">Strands Game</a></div><style dangerouslySetInnerHTML={{ __html: ".mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}" }} /></div>
+                <div className="mapouter">
+                    <div className="gmap_canvas">
+                        <iframe
+                            className="gmap_iframe"
+                            frameBorder={0}
+                            scrolling="no"
+                            marginHeight={0}
+                            marginWidth={0}
+                            src="https://maps.google.com/maps?width=600&height=400&hl=en&q=No.%204%20Canaan%20drive,%20Canaan%20Estate%20Life%20Camp%20Ext,%20Abuja&t=h&z=14&ie=UTF8&iwloc=B&output=embed"
+                        />
+                        <a href="https://strandsgame.net/">Strands Game</a>
+                    </div>
+                    <style
+                        dangerouslySetInnerHTML={{
+                            __html:
+                                ".mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}",
+                        }}
+                    />
+                </div>
 
                 <div className="row mt-5">
                     <div className="col-lg-4">
@@ -16,8 +73,7 @@ function Contact() {
                             <div className="address">
                                 <i className="bi bi-geo-alt" />
                                 <h4>Location:</h4>
-                                <p>No. 4 Canaan drive, Canaan Estate
-                                    Life Camp Ext, Abuja</p>
+                                <p>No. 4 Canaan drive, Canaan Estate Life Camp Ext, Abuja</p>
                             </div>
                             <div className="email">
                                 <i className="bi bi-envelope" />
@@ -27,19 +83,18 @@ function Contact() {
                             <div className="phone">
                                 <i className="bi bi-phone" />
                                 <h4>Call:</h4>
-                                <p>+234 803 704 5161
-                                </p>
+                                <p>+234 803 704 5161</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-8 mt-5 mt-lg-0">
-                        <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+                        <form className="php-email-form" ref={form} onSubmit={sendEmail}>
                             <div className="row">
                                 <div className="col-md-6 form-group">
-                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                                    <input type="text" name="user_name" className="form-control" id="name" placeholder="Your Name" required />
                                 </div>
                                 <div className="col-md-6 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                    <input type="email" className="form-control" name="user_email" id="email" placeholder="Your Email" required />
                                 </div>
                             </div>
                             <div className="form-group mt-3">
@@ -63,8 +118,7 @@ function Contact() {
                 </div>
             </div>
         </section>
-
-    )
+    );
 }
 
-export default Contact
+export default Contact;
